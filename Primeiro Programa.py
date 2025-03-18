512119ce68ad0c492e8249665a3dde6455d7061e
tulhy <arthurnovi@yahoo.com.br>
2025-03-17 22:47:28 -0300

    primeira aula em python

--- Primeiro Programa.py
+++ Primeiro Programa.py
@@ -1,1 +1,31 @@
-print("hello world!")
+# Exercício 1
+print("Arthur Augusto")
+print()
+
+# Exercício 2
+a = 3
+b = 5
+print("Cálculo 1")
+print(2*a*3*b)
+print()
+
+# Exercício 3
+# num1 = 2
+# num2 = 3
+# num3 = 4
+# soma = num1+num2+num3+
+# print(soma)
+
+num1 = float(input("defina numero 1 = "))
+num2 = float(input("defina numero 2 = "))
+num3 = float(input("defina numero 3 = "))
+soma = num1+num2+num3
+print(f"soma = {soma:.2f}")
+
+# num1 = int(input("defina numero 1 = "))
+# num2 = int(input("defina numero 2 = "))
+# num3 = int(input("defina numero 3 = "))
+# soma = num1+num2+num3
+# print(soma)
+
+
